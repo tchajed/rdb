de333e46c5e9e185cb9ae5bd13737eead068880e
Tej Chajed <tchajed@mit.edu>
2022-06-22 21:49:46 -0400

    Clean things up by moving ptrace stuff out

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,20 @@
 use std::{
+    collections::HashMap,
     ffi::{OsStr, OsString},
     io::{self, stdout, Error, Result, Write},
-    os::unix::prelude::CommandExt,
+    os::unix::process::CommandExt,
     process::{self, Stdio},
 };
 
-use std::collections::HashMap;
-
 use libc::pid_t;
 
-mod ptrace {
-    use libc::{c_uint, pid_t};
+mod ptrace;
 
-    const TRACEME: c_uint = 0;
-    const PEEKDATA: c_uint = 2;
-    const POKEDATA: c_uint = 5;
-    const CONT: c_uint = 7;
+use ptrace::WaitStatus;
 
-    pub unsafe fn trace_me() {
-        libc::ptrace(TRACEME);
-    }
-
-    pub unsafe fn cont(pid: pid_t, signal: c_uint) {
-        libc::ptrace(CONT, pid, 0, signal);
-    }
-
-    pub unsafe fn peekdata(pid: pid_t, addr: u64) -> u64 {
-        libc::ptrace(PEEKDATA, pid, addr) as u64
-    }
-
-    pub unsafe fn pokedata(pid: pid_t, addr: u64, data: u64) {
-        libc::ptrace(POKEDATA, pid, addr, data);
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum WaitStatus {
-    Exited(u8),
-    Signaled(u32),
-    Stopped(u32),
-}
-
-impl From<libc::c_int> for WaitStatus {
-    fn from(stat_val: libc::c_int) -> Self {
-        if libc::WIFEXITED(stat_val) {
-            WaitStatus::Exited(libc::WEXITSTATUS(stat_val) as u8)
-        } else if libc::WIFSIGNALED(stat_val) {
-            WaitStatus::Signaled(libc::WTERMSIG(stat_val) as u32)
-        } else if libc::WIFSTOPPED(stat_val) {
-            WaitStatus::Stopped(libc::WSTOPSIG(stat_val) as u32)
-        } else {
-            panic!("unexpected wait status");
-        }
-    }
-}
-
-fn wait(pid: pid_t) -> WaitStatus {
-    let mut status = 0;
-    unsafe { libc::waitpid(pid, &mut status, 0) };
-    return WaitStatus::from(status);
-}
-
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Breakpoint {
-    child: pid_t,
+    child: ptrace::Target,
     addr: u64,
     saved_data: Option<u8>,
 }
@@ -71,7 +23,7 @@ impl Breakpoint {
     // x86 int $3
     const INT_INSTR: u8 = 0xcc;
 
-    pub fn new(child: pid_t, addr: u64) -> Self {
+    pub fn new(child: ptrace::Target, addr: u64) -> Self {
         Self {
             child,
             addr,
@@ -85,36 +37,32 @@ impl Breakpoint {
 
     pub fn enable(&mut self) {
         assert!(!self.enabled(), "breakpoint is already enabled");
-        let old_data = unsafe { ptrace::peekdata(self.child, self.addr) };
+        let old_data = unsafe { self.child.peekdata(self.addr) };
         let saved = (old_data & 0xff) as u8;
         self.saved_data = Some(saved);
         let new_data = (old_data & (!0xff)) | (Self::INT_INSTR as u64);
-        unsafe { ptrace::pokedata(self.child, self.addr, new_data) };
+        unsafe { self.child.pokedata(self.addr, new_data) };
     }
 
     pub fn disable(&mut self) {
         assert!(self.enabled(), "breakpoint is not enabled");
-        let old_data = unsafe { ptrace::peekdata(self.child, self.addr) };
+        let old_data = unsafe { self.child.peekdata(self.addr) };
         let new_data = (old_data & (!0xff)) | (self.saved_data.unwrap() as u64);
-        unsafe { ptrace::pokedata(self.child, self.addr, new_data) };
+        unsafe { self.child.pokedata(self.addr, new_data) };
     }
 }
 
 struct Dbg {
-    child: pid_t,
+    child: ptrace::Target,
     breakpoints: HashMap<u64, Breakpoint>,
 }
 
 impl Dbg {
     fn new(child: pid_t) -> Self {
         Self {
-            child,
+            child: ptrace::Target::new(child),
             breakpoints: HashMap::new(),
         }
-    }
-
-    fn wait(&self) -> WaitStatus {
-        wait(self.child)
     }
 
     fn handle_command(&mut self, line: String) {
@@ -150,9 +98,9 @@ impl Dbg {
     }
 
     fn continue_execution(&self) {
-        unsafe { ptrace::cont(self.child, 0) };
+        unsafe { self.child.cont(0) };
 
-        match self.wait() {
+        match self.child.wait() {
             WaitStatus::Exited(status) => {
                 if status == 0 {
                     println!("program exited");
@@ -192,7 +140,7 @@ impl Dbg {
     fn run(&mut self) -> Result<()> {
         println!("debugging pid {}", self.child);
 
-        match self.wait() {
+        match self.child.wait() {
             WaitStatus::Exited(_) => {
                 eprintln!("debugee exited");
             }
---
+++ src/ptrace.rs
@@ -0,0 +1,66 @@
+use std::fmt::Display;
+
+use libc::{c_uint, pid_t};
+
+const TRACEME: c_uint = 0;
+const PEEKDATA: c_uint = 2;
+const POKEDATA: c_uint = 5;
+const CONT: c_uint = 7;
+
+pub unsafe fn trace_me() {
+    libc::ptrace(TRACEME);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Target(pid_t);
+
+impl Display for Target {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum WaitStatus {
+    Exited(u8),
+    Signaled(u32),
+    Stopped(u32),
+}
+
+impl From<libc::c_int> for WaitStatus {
+    fn from(stat_val: libc::c_int) -> Self {
+        if libc::WIFEXITED(stat_val) {
+            WaitStatus::Exited(libc::WEXITSTATUS(stat_val) as u8)
+        } else if libc::WIFSIGNALED(stat_val) {
+            WaitStatus::Signaled(libc::WTERMSIG(stat_val) as u32)
+        } else if libc::WIFSTOPPED(stat_val) {
+            WaitStatus::Stopped(libc::WSTOPSIG(stat_val) as u32)
+        } else {
+            panic!("unexpected wait status");
+        }
+    }
+}
+
+impl Target {
+    pub fn new(pid: pid_t) -> Self {
+        Self(pid)
+    }
+
+    pub unsafe fn cont(&self, signal: c_uint) {
+        libc::ptrace(CONT, self.0, 0, signal);
+    }
+
+    pub unsafe fn peekdata(&self, addr: u64) -> u64 {
+        libc::ptrace(PEEKDATA, self.0, addr) as u64
+    }
+
+    pub unsafe fn pokedata(&self, addr: u64, data: u64) {
+        libc::ptrace(POKEDATA, self.0, addr, data);
+    }
+
+    pub fn wait(&self) -> WaitStatus {
+        let mut status = 0;
+        unsafe { libc::waitpid(self.0, &mut status, 0) };
+        return WaitStatus::from(status);
+    }
+}
