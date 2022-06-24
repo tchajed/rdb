3929f3d8904afb653c4ad1e05bf730b8c1f0d08a
Tej Chajed <tchajed@mit.edu>
2022-06-24 12:23:23 -0400

    Remove dead code

--- src/dwarf.rs
+++ src/dwarf.rs
@@ -1,47 +1,8 @@
 #![allow(unused_variables)]
 use std::{borrow::Cow, ops::Range, rc::Rc};
 
-use gimli::{AttributeValue, DebuggingInformationEntry, EndianRcSlice, LittleEndian};
+use gimli::{EndianRcSlice, LittleEndian};
 use object::{File, Object, ObjectSection};
-
-type Die<'abbrev, 'unit, R> =
-    DebuggingInformationEntry<'abbrev, 'unit, R, <R as gimli::Reader>::Offset>;
-
-fn at_low_pc<R: gimli::Reader>(die: &Die<R>) -> gimli::Result<u64> {
-    let low_pc = die
-        .attr(gimli::DW_AT_low_pc)?
-        .map(|attr| {
-            if let AttributeValue::Addr(a) = attr.value() {
-                a
-            } else {
-                panic!("invalid low pc type")
-            }
-        })
-        .unwrap_or(0);
-    Ok(low_pc)
-}
-
-fn at_high_pc(low_pc: u64, high_pc: AttributeValue<impl gimli::Reader>) -> u64 {
-    match high_pc {
-        AttributeValue::Addr(a) => a,
-        AttributeValue::Sdata(n) => low_pc + n as u64,
-        AttributeValue::Udata(n) => low_pc + n as u64,
-        _ => panic!("invalid high_pc type"),
-    }
-}
-
-fn at_pc_range(die: &Die<impl gimli::Reader>) -> gimli::Result<Range<u64>> {
-    let low_pc = at_low_pc(die)?;
-    let high_pc = die
-        .attr(gimli::DW_AT_high_pc)?
-        .map(|a| at_high_pc(low_pc, a.value()))
-        .unwrap_or(low_pc + 1);
-    Ok(low_pc..high_pc)
-}
-
-// fn unit_pc_range(unit: &Unit<impl gimli::Reader>) -> gimli::Result<Range<u64>> {
-//     at_pc_range(unit.entries_tree(None)?.root()?.entry())
-// }
 
 // the gimli::Reader we use
 type Reader = EndianRcSlice<LittleEndian>;
